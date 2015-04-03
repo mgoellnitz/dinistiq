a0b407403cab85dfe6c4a2daa3c20737c560808c
Martin Goellnitz <martin@goellnitz.de>
2015-04-03 20:10:54 +0200

    Why not use mockito?

--- src/test/java/dinistiq/web/test/MockServletContext.java
+++ src/test/java/dinistiq/web/test/MockServletContext.java
@@ -24,25 +24,22 @@ import dinistiq.web.DinistiqContextLoaderListener;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Collection;
 import java.util.Enumeration;
 import java.util.EventListener;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
-import javax.servlet.MultipartConfigElement;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-import javax.servlet.ServletSecurityElement;
 import javax.servlet.SessionCookieConfig;
 import javax.servlet.SessionTrackingMode;
 import javax.servlet.descriptor.JspConfigDescriptor;
+import org.mockito.Mockito;
 
 
 @SuppressWarnings("deprecation")
@@ -250,92 +247,7 @@ public class MockServletContext implements ServletContext {
 
     @Override
     public ServletRegistration.Dynamic addServlet(String string, Servlet srvlt) {
-        return new ServletRegistration.Dynamic() {
-
-
-            @Override
-            public void setLoadOnStartup(int i) {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public Set<String> setServletSecurity(ServletSecurityElement sse) {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public void setMultipartConfig(MultipartConfigElement mce) {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public void setRunAsRole(String string) {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public Set<String> addMapping(String... strings) {
-                return new HashSet<>();
-            }
-
-
-            @Override
-            public Collection<String> getMappings() {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public String getRunAsRole() {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public String getName() {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public String getClassName() {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public boolean setInitParameter(String string, String string1) {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public String getInitParameter(String string) {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public Set<String> setInitParameters(Map<String, String> map) {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public Map<String, String> getInitParameters() {
-                throw new UnsupportedOperationException("NYI");
-            }
-
-
-            @Override
-            public void setAsyncSupported(boolean arg0) {
-                throw new UnsupportedOperationException("NYI");
-            }
-        };
+        return Mockito.mock(ServletRegistration.Dynamic.class);
     }
 
 
